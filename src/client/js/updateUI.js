832490c9c2be0d12537cd6e9fb0c565f7e0ae2ea
Toby Jorris <tobyjorris@gmail.com>
2020-10-01 11:43:30 -0500

    trip card styling works on xl screens

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -6,20 +6,30 @@ function updateUI(trip) {
 
     for (const day of trip.weather) {
         const weatherDay = document.createElement('li');
-        weatherDay.classList.add('weatherDay');
-        weatherDay.textContent = `${day.date}: low: ${day.low} | high: ${day.high}`;
+        if (trip.weather.length > 1) {
+            weatherDay.classList.add('weatherIsList');
+        } else {
+            weatherDay.classList.add('weatherDay')
+        }
+
+        weatherDay.textContent = `${day.date} - low temp: ${day.low} | high temp: ${day.high}`;
         weatherList.appendChild(weatherDay)
     }
 
     console.log(weatherList)
 
     tripHolder.innerHTML = `
-        <div class="tripDate">${trip.departureDate}</div>
-        <div class="tripDestination">${trip.cityName} ${trip.state}</div>
-        <img class="tripImage" src="${trip.imageURL}"  alt="Picture of ${trip.cityName}">
-        <div class="weatherHolder">Weather Forecast:</div> 
-        <button type="submit" onsubmit="localStorageService.writeToLocalStorage(${trip})" class="save btn">Save Trip</button>
-        <button class="cancel btn">Cancel Trip</button>
+        <div class="tripCardBackground">
+            <div class="tripDateTitle">Departure Date: </div>
+            <div class="tripDate">${trip.departureDate}</div>
+            <div class="tripDestinationTitle">Destination:</div>
+            <div class="tripDestination">${trip.cityName} ${trip.state}</div>
+            <img class="tripImage" src="${trip.imageURL}"  alt="Picture of ${trip.cityName}">
+            <div class="weatherHolder">Weather Forecast:</div> 
+            <button type="submit" onsubmit="localStorageService.writeToLocalStorage(${trip})" class="save btn">Save Trip</button>
+            <button class="cancel btn">Cancel Trip</button>
+        </div>
+        
     `
 
     const weatherHolder = document.querySelector('.weatherHolder')
